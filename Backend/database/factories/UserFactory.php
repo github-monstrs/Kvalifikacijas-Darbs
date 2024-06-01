3025cfb18d1e20cf59cd7baacad2f63e19aab17a
22DP3JZorg <51489832+github-monstrs@users.noreply.github.com>
2024-06-01 18:09:58 +0300

    Uzstaisīju lai pirmais lietotājs būtu administrātors

--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -26,10 +26,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'is_admin' => true,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('admin123'),
             'remember_token' => Str::random(10),
         ];
+        
     }
 
     /**
